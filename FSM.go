bd260087d3a6bfad26600b187cb4e66ea5254e06
Phil Hanna <ph1204@gmail.com>
2023-05-30 21:15:04 -0400

    Added events and delta function elements

--- FSM.go
+++ FSM.go
@@ -6,4 +6,26 @@ package fsm
 
 // FSM is a finite-state machine
 type FSM struct {
+	// The set of events that this FSM can respond to
+	events []Event
+
+	// The function that maps a tuple of state and event to
+	// the transition that happens.
+	deltaFunction func(State, Event) Transition
 }
+
+// ---------------------------------------------------------------------
+// Constructors
+// ---------------------------------------------------------------------
+
+func NewFSM() *FSM {
+	fsm := new(FSM)
+	fsm.events = nil
+	fsm.deltaFunction = nil
+	return fsm
+}
+
+// ---------------------------------------------------------------------
+// Methods
+// ---------------------------------------------------------------------
+
