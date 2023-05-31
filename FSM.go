c1382e2a11e8d2fee35a4f438d3d9e6f09a1345d
Phil Hanna <ph1204@gmail.com>
2023-05-30 21:17:11 -0400

    Finished structure

--- FSM.go
+++ FSM.go
@@ -6,12 +6,19 @@ package fsm
 
 // FSM is a finite-state machine
 type FSM struct {
+
 	// The set of events that this FSM can respond to
 	events []Event
 
 	// The function that maps a tuple of state and event to
 	// the transition that happens.
 	deltaFunction func(State, Event) Transition
+
+	// The initial state
+	initialState State
+
+	// The set of final states
+	finalStates []State
 }
 
 // ---------------------------------------------------------------------
@@ -22,6 +29,8 @@ func NewFSM() *FSM {
 	fsm := new(FSM)
 	fsm.events = nil
 	fsm.deltaFunction = nil
+	fsm.initialState = *new(State)
+	fsm.finalStates = make([]State, 0)
 	return fsm
 }
 
