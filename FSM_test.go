d1125fdbcf92733a73da8e0b4d2bf5071ef84c03
Phil Hanna <ph1204@gmail.com>
2023-06-07 11:11:24 -0400

    Made functions external to test

--- FSM_test.go
+++ FSM_test.go
@@ -22,40 +22,6 @@ func TestFSM_DivisibleBy3(t *testing.T) {
 		q0, q1, q2,
 	}
 
-	F0 := func(event Event[int]) (State, error) {
-
-		switch event {
-		case '1', '4', '7':
-			return q1, nil
-		case '2', '5', '8':
-			return q2, nil
-		default:
-			return q0, nil
-		}
-	}
-
-	F1 := func(event Event[int]) (State, error) {
-		switch event {
-		case '1', '4', '7':
-			return q2, nil
-		case '2', '5', '8':
-			return q0, nil
-		default:
-			return q1, nil
-		}
-	}
-
-	F2 := func(event Event[int]) (State, error) {
-		switch event {
-		case '1', '4', '7':
-			return q0, nil
-		case '2', '5', '8':
-			return q1, nil
-		default:
-			return q2, nil
-		}
-	}
-
 	fsm.TransitionMap = map[State]Transition[int]{
 		q0: F0,
 		q1: F1,
@@ -66,10 +32,44 @@ func TestFSM_DivisibleBy3(t *testing.T) {
 	inch := make(chan Event[int])
 	ouch := fsm.Run(inch)
 	var state State
-	for _, r := range "11" {
+	for _, r := range "333" {
 		inch <- Event[int](r)
 		state = <-ouch
 	}
 	log.Printf("Final state=%d\n", state)
 
+}
+
+func F0(event Event[int]) (State, error) {
+
+	switch event {
+	case '1', '4', '7':
+		return q1, nil
+	case '2', '5', '8':
+		return q2, nil
+	default:
+		return q0, nil
+	}
+}
+
+func F1(event Event[int]) (State, error) {
+	switch event {
+	case '1', '4', '7':
+		return q2, nil
+	case '2', '5', '8':
+		return q0, nil
+	default:
+		return q1, nil
+	}
+}
+
+func F2(event Event[int]) (State, error) {
+	switch event {
+	case '1', '4', '7':
+		return q0, nil
+	case '2', '5', '8':
+		return q1, nil
+	default:
+		return q2, nil
+	}
 }
